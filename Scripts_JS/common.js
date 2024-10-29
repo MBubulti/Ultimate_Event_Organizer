8a7d427a33c2ca0b5b93559976d08b2385ba217d
Maxime Buffet <167359993+MBubulti@users.noreply.github.com>
2024-10-29 10:48:35 +0100

    MAJ HTML
    Barre Nav unifiée

--- Scripts_JS/common.js
+++ Scripts_JS/common.js
@@ -2,11 +2,21 @@
 
 //Mise en place un bouton d'identification utilisateur//
 //Etape 1 - Identifier lorsqu'un clic est effectué sur le bouton
-let boutonConnexion = document.getElementById("boutonConnexion")
-boutonConnexion.addEventListener("click", ()=>{
+let btnConnexion = document.getElementById("btnConnexion")
+btnConnexion.addEventListener("click", ()=>{
     console.log("CLICK")
 })
 
-
+//Etape 2 -  Reconnaitre les éléments renseignés en identifiant & mdp de connexion
+let input_identifiant = document.getElementById("nomConnexion")
+let  input_motDePasse = document.getElementById("motDePasse")
+const btnEnvoiConnexion = document.querySelector("form")
+btnEnvoiConnexion.addEventListener("submit",  (connexion) =>{
+    connexion.preventDefault() //prévention rechargement de la page au  submit
+    let identifiant = input_identifiant.value
+    let motDePasse = input_motDePasse.value
+    console.log(identifiant + motDePasse)
+    console.log("pas de rechargement")
+})
 
 
